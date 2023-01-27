ce5ca110a1b73c4f8e46d88ac7677ecc4ca2a3ae
Garabola <rtolojanaharyjasmin@gmail.com>
2023-01-27 05:32:17 +0300

    Switch theme with React context API

--- src/components/ThemeToggle/Component.tsx
+++ src/components/ThemeToggle/Component.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 import './styles.css';
 
 export default function Component() {
   const [checked, setChecked] = React.useState(true);
+  const {setTheme,theme} = useContext(ThemeContext)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log("Oops!, doesn't work, check /challenges/06-switch-theme to solve this issue");
     setChecked(e.target.checked);
+    if(checked){
+      setTheme('dark')
+    }else{
+      setTheme('light')
+    }
   };
 
   return (
--- src/components/contexts/ThemeContext.tsx
+++ src/components/contexts/ThemeContext.tsx
@@ -1,27 +1,32 @@
 'use client';
 
-import { createContext, useContext, PropsWithChildren } from 'react';
+import { createContext, useContext, PropsWithChildren, useState } from 'react';
 
 export declare type ThemeValue = 'dark' | 'light';
 
 export interface ThemeContextValue<T extends string> {
   theme: T;
+  setTheme: (val : T)=>void
 }
 
 interface ThemeProviderProps<T> {
   defaultTheme?: T;
+  
 }
 
 export const ThemeContext = createContext<ThemeContextValue<ThemeValue>>({
   theme: 'light',
+  setTheme: ()=>{}
 });
 
 export const ThemeProvider = ({ children, defaultTheme }: PropsWithChildren<ThemeProviderProps<ThemeValue>>) => {
+  const [theme,setTheme ] = useState<ThemeValue>(defaultTheme || 'light')
   return (
     <ThemeContext.Provider
       value={{
         // @todo: handle this with a state, that can toggle between dark and light
-        theme: defaultTheme || 'light',
+        setTheme,
+        theme,
       }}
     >
       {children}
